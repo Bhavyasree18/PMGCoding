f8b9bb9fde586c04a6765cf1efd2b415286d7923
ramu450 <kodandaramu450@gmail.com>
2022-02-12 13:50:37 -0600

    Succesfully Completed csv-combiner task

---
+++ CSVFilesMerger.java
@@ -0,0 +1,52 @@
+import java.lang.*;
+import java.nio.file.*;
+import java.nio.charset.*;
+import java.io.*;
+import java.util.*;
+
+public class CSVFilesMerger{
+	public static void main(String[] args) {
+		if(args.length <2)  // Check atleast two files to merge
+		{
+		    System.out.println("Atleast two files need to be passed as argumwnts");
+		}
+		List<Path> paths = new ArrayList<>();
+		for(String arg : args){
+			if(Files.exists(Paths.get(arg)) && Paths.get(arg).getFileName().toString().contains(".csv")) { // Check whether the file exists in the path and also file format should be .csv
+				paths.add(Paths.get(arg)); 
+			}else {
+				System.out.println(arg + " : FILE NOT SUPPORTED");
+			}
+		}
+		
+		mergeFile(paths); // Calling mergeFile Function
+	}
+
+	private static void mergeFile(List<Path> paths) {
+		
+		try{
+		    int count = 1;
+		for(Path p: paths)
+		{
+		    BufferedReader br = Files.newBufferedReader(p, StandardCharsets.UTF_8); // Buffer Reader declaration
+		    String line = br.readLine();
+		    if(count != 0){ // Insert Header only Once
+		    	System.out.println(line +", filename"+System.lineSeparator());  // writing in to file
+		        count--;
+		    }
+		    while((line = br.readLine()) != null)  // Read Untill the End Line
+		    {
+		    	System.out.println(line+", "+p.getFileName()+System.lineSeparator()); // writing in to file
+		    }
+		    br.close();
+		}
+		}
+		catch(IOException e){
+		    System.out.println("Something is interrupted with the some error!!!!!");
+		    e.printStackTrace();
+		}
+		
+		return ;
+		
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # PMGCoding
+
 Coding task for PMG
+
+# CSV Combiner using java
+
+Steps to follow
+
+1. Assume that all the files have same headers and Same number of Columns.
+
+2. Paste the .csv files in the folder of the .java file.
+
+3. Open the Terminal where the .java file is placed.
+
+3. compile the java program using the below command
+
+	 "javac CSVFilesMerger.java"
+	 
+4. .class will be created after you compile.
+
+5. Run the .class file using the below command
+
+	"java -cp . CSVFilesMerger accessories.csv clothing.csv household_cleaners.csv  > output.csv "
+	
+# Completed
+
